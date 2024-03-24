714190d0763f10b02d0068f43c69f53ef6f4f6dd
plwast-ints <plwast.ints@gmail.com>
2024-03-25 04:50:25 +0900

    팝업북 넘기기 테스트 - 관련 이슈: #3

---
+++ Unity-Re_less/Assets/_Reless/Scripts/Reless/PopupBook.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Reless
+{
+    /// <summary>
+    /// 팝업북과 관련된 동작을 담당하는 클래스입니다.
+    /// <remarks> 현재 구조화 중, 구현 예정 </remarks> 
+    /// </summary>
+    public class PopupBook : MonoBehaviour
+    {
+        /// <summary>
+        /// 현재 펼쳐진 페이지의 쪽수를 나타냅니다.
+        /// </summary>
+        private int _currentPageIndex = 0;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
