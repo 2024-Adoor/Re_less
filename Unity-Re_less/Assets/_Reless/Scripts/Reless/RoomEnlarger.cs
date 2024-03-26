bde2617b140a63a37a7b34736d85d5e7e2ee863f
plwast-ints <plwast.ints@gmail.com>
2024-03-27 04:08:20 +0900

    RoomEnlarger 스크립트 생성 - RoomEnlarger 스크립트로 방 키우는 애니메이션에 대한 값을 바꿀 수 있는 컴포넌트 만드는 중. 관련 이슈: #4 - MixedReality 씬 생성

---
+++ Unity-Re_less/Assets/_Reless/Scripts/Reless/RoomEnlarger.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using UnityEngine;
+using Meta.XR.MRUtilityKit;
+using Reless;
+using UnityEditor;
+
+namespace Reless
+{
+    
+    /// <summary>
+    /// 작성 중
+    /// </summary>
+    public class RoomEnlarger : MonoBehaviour
+    {
+        private MRUKRoom _room;
+        
+        /// <summary>
+        /// 방의 크기를 키울 때 사용할 크기 배율입니다.
+        /// </summary>
+        [SerializeField]
+        private float enlargedScale;
+
+        /// <summary>
+        /// 방이 커지는 애니메이션이 진행되는 시간(초)입니다.
+        /// </summary>
+        [SerializeField]
+        private float enlargingDuration;
+        
+        [SerializeField]
+        private AnimationCurve animationCurve;
+        
+        private void OnValidate()
+        {
+            if (enlargedScale == default) enlargedScale = 15f;
+            if (enlargingDuration == default) enlargingDuration = 2f;
+        }
+
+        /// <summary>
+        /// 방의 크기를 키웁니다.
+        /// </summary>
+        public void EnlargeRoom()
+        {
+            StartCoroutine(EnlargingRoom());
+            
+            IEnumerator EnlargingRoom()
+            {
+                // 방의 크기를 키우는 애니메이션
+                for (float elapsedTime = 0f; elapsedTime < enlargingDuration; elapsedTime += Time.deltaTime)
+                {
+                    float scale = Mathf.Lerp(0.1f, 1.0f, animationCurve.Evaluate(elapsedTime / enlargingDuration)) * enlargedScale;
+                    _room.transform.localScale = Vector3.one * scale;
+                    yield return null;
+                }
+                
+                // 최종 값으로 방의 크기 변경
+                _room.transform.localScale = Vector3.one * enlargedScale;
+            }
+        }
+        
+        public void Initialize()
+        {
+            _room = FindObjectOfType<MRUKRoom>();
+        }
+    }
+}
+
+[CustomEditor(typeof(RoomEnlarger))]
+public class RoomEnlargerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        RoomEnlarger roomEnlarger = (RoomEnlarger)target;
+
+        if (GUILayout.Button("Run EnlargeRoom() for Testing"))
+        {
+            roomEnlarger.EnlargeRoom();
+        }
+    }
+}
