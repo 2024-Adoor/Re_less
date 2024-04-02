5d08587fd2e9aa46b6e378fbf3da6a75f05b6f24
plwast-ints <plwast.ints@gmail.com>
2024-04-03 02:04:49 +0900

    PageUnfolding.cs 주석 일부 추가

--- Unity-Re_less/Assets/Scripts/PageUnfolding.cs
+++ Unity-Re_less/Assets/Scripts/PageUnfolding.cs
@@ -1,21 +1,22 @@
-using System;
-using NaughtyAttributes;
 using Oculus.Interaction;
 using UnityEngine;
 
+/// <summary>
+/// 페이지가 공중에서 놓아졌을 때 펼쳐지게 하는 클래스입니다.
+/// </summary>
 public class PageUnfolding : MonoBehaviour
 {
     [SerializeField]
-    private InteractableGroupView _interactableGroupView;
+    private InteractableGroupView interactableGroupView;
     
     [SerializeField]
-    private float _speed;
+    private float speed;
 
     [SerializeField] 
-    private bool _left;
+    private bool left;
     
     [SerializeField]
-    private float _acceleration = 0.2f;
+    private float acceleration = 0.2f;
     
     private float _accelerationTime;
     
@@ -23,33 +24,35 @@ public class PageUnfolding : MonoBehaviour
 
     private void LateUpdate()
     {
-        if (_interactableGroupView.State == InteractableState.Select)
+        // 플레이어가 페이지를 잡고 있나요?
+        if (interactableGroupView.State == InteractableState.Select)
         {
-            if (_left ? transform.up.x < 0 : transform.up.x > 0)
+            // 페이지의 회전각을 책 위로 제한합니다.
+            if (left ? transform.up.x < 0 : transform.up.x > 0)
             {
-                if (_left ? transform.forward.x < 0 : transform.forward.x > 0)
+                if (left ? transform.forward.x < 0 : transform.forward.x > 0)
                 {
-                    transform.rotation = Quaternion.Euler(0f, _left ? -90f: 90f, 0f);
+                    transform.rotation = Quaternion.Euler(0f, left ? -90f: 90f, 0f);
                 }
                 else
                 {
-                    transform.rotation = Quaternion.Euler(-180f, _left ? -90f: 90f, 0f);
+                    transform.rotation = Quaternion.Euler(-180f, left ? -90f: 90f, 0f);
                 }
             }
             _accelerationTime = 0;
             return;
         }
 
-        _accelerationTime += _acceleration;
+        _accelerationTime += acceleration;
             
-
-        if (_left ? transform.forward.x < 0 : transform.forward.x > 0)
+        // 페이지가 놓아졌을 때 펼쳐지게 합니다.
+        if (left ? transform.forward.x < 0 : transform.forward.x > 0)
         {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0f, _left ? -90f: 90f, 0f), Time.deltaTime * _speed * _accelerationTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0f, left ? -90f: 90f, 0f), Time.deltaTime * speed * _accelerationTime);
         }
         else
         {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(-180f, _left ? -90f: 90f, 0f), Time.deltaTime * _speed * _accelerationTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(-180f, left ? -90f: 90f, 0f), Time.deltaTime * speed * _accelerationTime);
         }
     }
 }
