457072ad194951a0969d15fb5f1744eb6aaa96ee
plwast-ints <plwast.ints@gmail.com>
2024-04-04 03:51:52 +0900

    ChangeMaterialColor.cs 스크립트를 작성 - 매번 테스트 용도로 색만 다른 머티리얼을 새로 생성하지 않기 위해 만들었음.

---
+++ Unity-Re_less/Assets/Test/Scripts/ChangeMaterialColor.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+namespace Test
+{
+    /// <summary>
+    /// 테스트 시각화 목적으로 Material의 색상을 변경하는 클래스입니다.
+    /// </summary>
+    [RequireComponent(typeof(Renderer))]
+    public class ChangeMaterialColor : MonoBehaviour
+    {
+        [SerializeField]
+        private Color materialColor;
+
+        // Renderer 컴포넌트 참조
+        private Renderer renderer;
+
+        void Start()
+        {
+            // Renderer 컴포넌트 가져오기
+            renderer = GetComponent<Renderer>();
+            renderer.material.color = materialColor;
+        }
+
+        private void OnValidate()
+        {
+            if (Application.isPlaying)
+            {
+                renderer.material.color = materialColor;
+            }
+        }
+    }
+}
+
