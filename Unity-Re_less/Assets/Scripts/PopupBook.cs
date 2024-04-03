6464acdb6259595e0438c4470a64d0387ea1c8e7
plwast-ints <plwast.ints@gmail.com>
2024-04-03 10:49:50 +0900

    PopupBook.cs 구현 중 - 관련 이슈: #8

--- Unity-Re_less/Assets/Scripts/PopupBook.cs
+++ Unity-Re_less/Assets/Scripts/PopupBook.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using Oculus.Interaction;
 using UnityEngine;
 
 namespace Reless
@@ -17,66 +14,42 @@ namespace Reless
         /// </summary>
         private int _currentPageIndex = 0;
         
-        private List<GameObject> _pages = new List<GameObject>();
+        [SerializeField]
+        private List<GameObject> leftPopups = new List<GameObject>();
         
-        private InteractableGroupView _leftInteractable;
-        private InteractableGroupView _rightInteractable;
-
-        private float _foldingSpeedAcceleration;
+        [SerializeField]
+        private List<GameObject> rightPopups = new List<GameObject>();
         
-        // Start is called before the first frame update
-        void Start()
-        {
+        [SerializeField]
+        private GameObject leftPage;
         
-        }
+        [SerializeField]
+        private GameObject rigthPage;
 
-        private void OnEnable()
+        // TODO:
+        
+        public void TurnNextPage()
         {
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            /*if (_leftInteractable.State != InteractableState.Select)
-            {
-                if (_leftInteractable.gameObject.transform.rotation.x is > 0 and < 90)
-                {
-                    StartCoroutine(Folding(_leftInteractable.gameObject.transform, 0));
-
-                }
-                
-                
-            }*/
-        }
-
-        private IEnumerator Folding(Transform transform, float targetAngle)
-        {
-            // 각도 차이에 따라 회전 방향이 다릅니다.
-            float angleDifference = targetAngle - transform.rotation.x;
-            bool clockwise = angleDifference < 0;
-            float delta = Time.deltaTime;
+            _currentPageIndex++;
             
-            while (true)
-            {
-                transform.Rotate(Vector3.right, delta);
-
-                if (clockwise)
-                {
-                    if (transform.rotation.x > targetAngle) break;
-
-                    delta += _foldingSpeedAcceleration;
-                }
-                else
-                {
-                    if (transform.rotation.x < targetAngle) break;
-                    
-                    delta -= _foldingSpeedAcceleration;
-                }
-                
-                yield return null;
-            }
+            var leftPagePopup = Instantiate(leftPopups[_currentPageIndex], leftPage.transform);
+            var leftPageBackPopup = Instantiate(rightPopups[_currentPageIndex - 1], rigthPage.transform);
+            leftPageBackPopup.transform.Rotate(0, 180, 0);
+            var rightPagePopup = Instantiate(rightPopups[_currentPageIndex], rigthPage.transform);
+            var rightPageBackPopup = Instantiate(leftPopups[_currentPageIndex - 1], leftPage.transform);
+            rightPageBackPopup.transform.Rotate(0, 180, 0);
+        }
+        
+        public void TurnPreviousPage()
+        {
+            _currentPageIndex--;
             
-            transform.rotation = Quaternion.Euler(targetAngle, 0, 0);
+            var leftPagePopup = Instantiate(leftPopups[_currentPageIndex], leftPage.transform);
+            var leftPageBackPopup = Instantiate(rightPopups[_currentPageIndex + 1], rigthPage.transform);
+            leftPageBackPopup.transform.Rotate(0, 180, 0);
+            var rightPagePopup = Instantiate(rightPopups[_currentPageIndex], rigthPage.transform);
+            var rightPageBackPopup = Instantiate(leftPopups[_currentPageIndex + 1], leftPage.transform);
+            rightPageBackPopup.transform.Rotate(0, 180, 0);
         }
     }
 }
