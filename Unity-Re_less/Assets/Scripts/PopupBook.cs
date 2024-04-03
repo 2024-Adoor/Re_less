c21489d70424670611cbc89e642e94f0dd2f7716
plwast-ints <plwast.ints@gmail.com>
2024-04-04 07:36:44 +0900

    PopupBook.cs에서 페이지에 따라 팝업 눌리기, 팝업 생성 - 페이지 기준으로 팝업이 동적 생성되도록 구현 - 페이지 각도에 따라 다른 팝업 압력 애니메이션 - 관련 이슈: #8

--- Unity-Re_less/Assets/Scripts/PopupBook.cs
+++ Unity-Re_less/Assets/Scripts/PopupBook.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using NaughtyAttributes;
 using UnityEngine;
 
 namespace Reless
@@ -12,7 +15,37 @@ namespace Reless
         /// <summary>
         /// 현재 펼쳐진 페이지의 쪽수를 나타냅니다.
         /// </summary>
+        [ShowNonSerializedField]
         private int _currentPageIndex = 0;
+        
+        private int PageIndex
+        {
+            get => _currentPageIndex;
+            set
+            {
+                DeactivatePopups(_currentPageIndex);
+                _currentPageIndex = value;
+                ActivatePopups(_currentPageIndex);
+            }
+        }
+        
+        /// <summary>
+        /// 팝업북 페이지가 펼쳐지는 정도에 따라 팝업이 눌리는 정도를 조절하는 애니메이션 커브입니다.
+        /// </summary>
+        [SerializeField]
+        private AnimationCurve popupPressureCurve;
+        
+        [SerializeField]
+        private GameObject leftPage;
+        
+        [SerializeField]
+        private GameObject rightPage;
+
+        [SerializeField] 
+        private GameObject fixedLeftPage;
+        
+        [SerializeField]
+        private GameObject fixedRightPage;
         
         [SerializeField]
         private List<GameObject> leftPopups = new List<GameObject>();
@@ -21,35 +54,231 @@ namespace Reless
         private List<GameObject> rightPopups = new List<GameObject>();
         
         [SerializeField]
-        private GameObject leftPage;
+        private GameObject leftPopupsContainer;
         
         [SerializeField]
-        private GameObject rigthPage;
+        private GameObject rightPopupsContainer;
 
-        // TODO:
-        
-        public void TurnNextPage()
+        /// <summary>
+        /// 팝업북에 붙는 팝업들의 위치 유형입니다.
+        /// </summary>
+        private enum PopupPosition
         {
-            _currentPageIndex++;
+            FixedLeft, // 고정된 왼쪽 페이지에 붙은 팝업
+            LeftBack, // 왼쪽 페이지의 뒤쪽에 붙은 팝업
+            Left, // 왼쪽 페이지에 붙은 팝업
+            Right, // 오른쪽 페이지에 붙은 팝업
+            RightBack, // 오른쪽 페이지의 뒤쪽에 붙은 팝업
+            FixedRight // 고정된 오른쪽 페이지에 붙은 팝업
+        }
+
+        
+        /// <summary>
+        /// 해당하는 위치의 팝업을 가져옵니다.
+        /// </summary>
+        /// <param name="pageIndex">기준이 되는 페이지 인덱스</param>
+        /// <param name="popupPosition">가져올 팝업의 위치 유형</param>
+        /// <returns>팝업 GameObject</returns>
+        private GameObject GetPopup(int pageIndex, PopupPosition popupPosition) => popupPosition switch
+        {
+            PopupPosition.FixedLeft => leftPopups.ElementAtOrDefault(pageIndex - 1),
+            PopupPosition.LeftBack => rightPopups.ElementAtOrDefault(pageIndex - 1),
+            PopupPosition.Left => leftPopups.ElementAtOrDefault(pageIndex),
+            PopupPosition.Right => rightPopups.ElementAtOrDefault(pageIndex),
+            PopupPosition.RightBack => leftPopups.ElementAtOrDefault(pageIndex + 1),
+            PopupPosition.FixedRight => rightPopups.ElementAtOrDefault(pageIndex + 1),
+            _ => null
+        };
+        
+        private GameObject GetCurrentPopup(PopupPosition popupPosition) => GetPopup(_currentPageIndex, popupPosition);
+        
+        private void Start()
+        {
             
-            var leftPagePopup = Instantiate(leftPopups[_currentPageIndex], leftPage.transform);
-            var leftPageBackPopup = Instantiate(rightPopups[_currentPageIndex - 1], rigthPage.transform);
-            leftPageBackPopup.transform.Rotate(0, 180, 0);
-            var rightPagePopup = Instantiate(rightPopups[_currentPageIndex], rigthPage.transform);
-            var rightPageBackPopup = Instantiate(leftPopups[_currentPageIndex - 1], leftPage.transform);
-            rightPageBackPopup.transform.Rotate(0, 180, 0);
+        }
+
+        [ShowNativeProperty]
+        private float _leftPageRotationUpX => leftPage.transform.up.x;
+        
+        [ShowNativeProperty]
+        private float _leftPageRotationUpY => leftPage.transform.up.y;
+        
+        [ShowNativeProperty]
+        private float _leftPageRotationUpZ => leftPage.transform.up.z;
+        
+        [ShowNativeProperty]
+        private float _rightPageRotation => rightPage.transform.up.x;
+        
+        [ShowNativeProperty]
+        private float _rightPageRotationUpY => rightPage.transform.up.y;
+        
+        private void Update()
+        {
+            UpdatePopupsPressure();
+        }
+
+        /// <summary>
+        /// 왼쪽 페이지와 오른쪽 페이지가 펼쳐진 각도에 따라 각 위치에 있는 팝업이 펼쳐지는 정도를 조절합니다.
+        /// </summary>
+        private void UpdatePopupsPressure()
+        {
+            var leftUp = leftPage.transform.up;
+            var rightUp = rightPage.transform.up;
+            
+            float leftPageOpenAngle = Mathf.Clamp(leftUp.x, 0f, 1f);
+            float rightPageOpenAngle = Mathf.Clamp(-rightUp.x, 0f, 1f);
+            
+            float leftPressure = popupPressureCurve.Evaluate(leftPageOpenAngle);
+            float rightPressure = popupPressureCurve.Evaluate(rightPageOpenAngle);
+
+            // 수직(90도) 보다 더 열리게 펼쳐졌는지 여부. 
+            bool leftPageAngleUpperVertical = leftUp.y < 0;
+            bool rightPageAngleUpperVertical = rightUp.y < 0;
+
+            var fixedLeftPopup = GetCurrentPopup(PopupPosition.FixedLeft);
+            var leftBackPopup = GetCurrentPopup(PopupPosition.LeftBack);
+            var leftPopup = GetCurrentPopup(PopupPosition.Left);
+            var rightPopup = GetCurrentPopup(PopupPosition.Right);
+            var rightBackPopup = GetCurrentPopup(PopupPosition.RightBack);
+            var fixedRightPopup = GetCurrentPopup(PopupPosition.FixedRight);
+            
+            // 페이지를 넘기는 각도가 90도를 넘어가면 (왼쪽)
+            if (leftPageAngleUpperVertical)
+            {
+                // 자신 페이지 아래에 눌려 있던 팝업들은 이제 제약이 필요 없습니다 - 완전히 풀립니다.
+                if (fixedLeftPopup is not null) fixedLeftPopup.transform.localScale = Vector3.one;
+                if (leftBackPopup is not null) leftBackPopup.transform.localScale = Vector3.one;
+                
+                // 페이지를 완전히 넘겨가는 경우 자신 페이지와 반대쪽 페이지 위의 팝업은 점점 작아져야 합니다.
+                if (leftPopup is not null) leftPopup.transform.localScale = new Vector3(1f, leftPressure, 1f);
+                if (rightPopup is not null) rightPopup.transform.localScale = new Vector3(1f, leftPressure, 1f);
+            }
+            // 페이지를 넘기는 각도가 90도 이하라면
+            else
+            {
+                // 자신 페이지 아래의 팝업들은 눌려 있어야 합니다. 특히 완전히 펼쳐져 있는 경우에 아래에 숨겨져 있을 팝업들의 크기는 0입니다.
+                if (fixedLeftPopup is not null) fixedLeftPopup.transform.localScale = new Vector3(1f, leftPressure, 1f);
+                if (leftBackPopup is not null) leftBackPopup.transform.localScale = new Vector3(1f, leftPressure, 1f);
+                
+                // 90도 이하의 펼친 각도에서 자신 페이지의 팝업은 완전히 풀려 있습니다. - 단 상대 페이지 또한 펼쳐져 있어야 합니다.
+                if (leftPopup is not null && !rightPageAngleUpperVertical) leftPopup.transform.localScale = Vector3.one;
+            }
+
+            // 페이지를 넘기는 각도가 90도를 넘어가면 (오른쪽)
+            if (rightPageAngleUpperVertical)
+            {
+                // 자신 페이지 아래에 눌려 있던 팝업들은 이제 제약이 필요 없습니다 - 완전히 풀립니다.
+                if (rightPopup is not null) rightPopup.transform.localScale = Vector3.one;
+                if (rightBackPopup is not null) rightBackPopup.transform.localScale = Vector3.one;
+                
+                // 페이지를 완전히 넘겨가는 경우 자신 페이지와 반대쪽 페이지 위의 팝업은 점점 작아져야 합니다.
+                if (rightPopup is not null) rightPopup.transform.localScale = new Vector3(1f, rightPressure, 1f);
+                if (leftPopup is not null) leftPopup.transform.localScale = new Vector3(1f, rightPressure, 1f);
+            }
+            // 페이지를 넘기는 각도가 90도 이하라면
+            else
+            {
+                // 자신 페이지 아래의 팝업들은 눌려 있어야 합니다. 특히 완전히 펼쳐져 있는 경우에 아래에 숨겨져 있을 팝업들의 크기는 0입니다.
+                if (fixedRightPopup is not null) fixedRightPopup.transform.localScale = new Vector3(1f, rightPressure, 1f);
+                if (rightBackPopup is not null) rightBackPopup.transform.localScale = new Vector3(1f, rightPressure, 1f);
+                
+                // 90도 이하의 펼친 각도에서 자신 페이지의 팝업은 완전히 풀려 있습니다. - 단 상대 페이지 또한 펼쳐져 있어야 합니다.
+                if (rightPopup is not null && !leftPageAngleUpperVertical) rightPopup.transform.localScale = Vector3.one;
+            }
+        }
+
+        public void TurnNextPage() => PageIndex++;
+        
+        public void TurnPreviousPage() => PageIndex--;
+
+        private static void ActivatePopup(GameObject popup, GameObject page)
+        {
+            popup.SetActive(true);
+            popup.transform.SetParent(page.transform);
+        }
+
+        private void ActivatePopups(int pageIndex)
+        {
+            var fixedLeftPopup = GetPopup(pageIndex, PopupPosition.FixedLeft);
+            var leftBackPopup = GetPopup(pageIndex, PopupPosition.LeftBack);
+            var leftPopup = GetPopup(pageIndex, PopupPosition.Left);
+            var rightPopup = GetPopup(pageIndex, PopupPosition.Right);
+            var rightBackPopup = GetPopup(pageIndex, PopupPosition.RightBack);
+            var fixedRightPopup = GetPopup(pageIndex, PopupPosition.FixedRight);
+
+            if (fixedLeftPopup is not null)
+            {
+                // 이전에 페이지 뒤에 붙는 팝업으로 사용되었다면 회전이 바뀌었을 수 있으니 부모 지정 전에 회전값을 초기화합니다.
+                fixedLeftPopup.transform.localRotation = Quaternion.identity;
+                ActivatePopup(fixedLeftPopup, fixedLeftPage);
+            }
+            if (leftBackPopup is not null)
+            {
+                ActivatePopup(leftBackPopup, leftPage);
+                // 페이지 뒤쪽에 붙는 팝업은 뒤집어야 합니다.
+                leftBackPopup.transform.localRotation = Quaternion.Euler(180, -90, 0);
+            }
+            if (leftPopup is not null)
+            {
+                // 이전에 페이지 뒤에 붙는 팝업으로 사용되었다면 회전이 바뀌었을 수 있으니 부모 지정 전에 회전값을 초기화합니다.
+                leftPopup.transform.localRotation = Quaternion.identity;
+                ActivatePopup(leftPopup, leftPage);
+            }
+            if (rightPopup is not null)
+            {
+                // 이전에 페이지 뒤에 붙는 팝업으로 사용되었다면 회전이 바뀌었을 수 있으니 부모 지정 전에 회전값을 초기화합니다.
+                rightPopup.transform.localRotation = Quaternion.identity;
+                ActivatePopup(rightPopup, rightPage);
+            }
+            if (rightBackPopup is not null)
+            {
+                ActivatePopup(rightBackPopup, rightPage);
+                // 페이지 뒤쪽에 붙는 팝업은 뒤집어야 합니다.
+                rightBackPopup.transform.localRotation = Quaternion.Euler(180, 90, 0);
+            }
+            if (fixedRightPopup is not null)
+            {
+                // 이전에 페이지 뒤에 붙는 팝업으로 사용되었다면 회전이 바뀌었을 수 있으니 부모 지정 전에 회전값을 초기화합니다.
+                fixedRightPopup.transform.localRotation = Quaternion.identity;
+                ActivatePopup(fixedRightPopup, fixedRightPage);
+            }
         }
         
-        public void TurnPreviousPage()
+        private static void DeactivatePopup(GameObject popup, GameObject container)
         {
-            _currentPageIndex--;
-            
-            var leftPagePopup = Instantiate(leftPopups[_currentPageIndex], leftPage.transform);
-            var leftPageBackPopup = Instantiate(rightPopups[_currentPageIndex + 1], rigthPage.transform);
-            leftPageBackPopup.transform.Rotate(0, 180, 0);
-            var rightPagePopup = Instantiate(rightPopups[_currentPageIndex], rigthPage.transform);
-            var rightPageBackPopup = Instantiate(leftPopups[_currentPageIndex + 1], leftPage.transform);
-            rightPageBackPopup.transform.Rotate(0, 180, 0);
+            popup.SetActive(false);
+            popup.transform.SetParent(container.transform);
         }
+
+        private void DeactivatePopups(int pageIndex)
+        {
+            var fixedLeftPopup = GetPopup(pageIndex, PopupPosition.FixedLeft);
+            var leftBackPopup = GetPopup(pageIndex, PopupPosition.LeftBack);
+            var leftPopup = GetPopup(pageIndex, PopupPosition.Left);
+            var rightPopup = GetPopup(pageIndex, PopupPosition.Right);
+            var rightBackPopup = GetPopup(pageIndex, PopupPosition.RightBack);
+            var fixedRightPopup = GetPopup(pageIndex, PopupPosition.FixedRight);
+            
+            if (fixedLeftPopup is not null) DeactivatePopup(fixedLeftPopup, leftPopupsContainer);
+            if (leftBackPopup is not null) DeactivatePopup(leftBackPopup, rightPopupsContainer);
+            if (leftPopup is not null) DeactivatePopup(leftPopup, leftPopupsContainer);
+            if (rightPopup is not null) DeactivatePopup(rightPopup, rightPopupsContainer);
+            if (rightBackPopup is not null) DeactivatePopup(rightBackPopup, leftPopupsContainer);
+            if (fixedRightPopup is not null) DeactivatePopup(fixedRightPopup, rightPopupsContainer);
+        }
+        
+        #region Debug
+#if UNITY_EDITOR
+        [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+        [BoxGroup("Debug"), SerializeField, OnValueChanged(nameof(SetPageIndex_method)), ShowIf(nameof(IsPlayMode_))]
+        private int setPageIndex_;
+
+        [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+        private bool IsPlayMode_ => Application.isPlaying;
+
+        [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+        private void SetPageIndex_method() => PageIndex = setPageIndex_;
+#endif
+        #endregion
     }
 }
