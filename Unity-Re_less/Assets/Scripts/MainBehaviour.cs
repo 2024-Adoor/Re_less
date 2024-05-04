f7b36b758d7695121888cd8945f3540090fd06a1
plwast-ints <plwast.ints@gmail.com>
2024-05-05 01:15:50 +0900

    각 챕터별 그릴 오브젝트를 한번에 나오도록 수정

--- Unity-Re_less/Assets/Scripts/MainBehaviour.cs
+++ Unity-Re_less/Assets/Scripts/MainBehaviour.cs
@@ -52,7 +52,14 @@ namespace Reless
             {
                 // 펜 활성화
                 EnablePen();
-
+                
+                // 그릴 오브젝트 생성
+                SetupSketchObject(_sketchObjectPrefabs.chapter01, _obtainingObjectPrefabs.chapter01).transform.Translate(-1, 0, 0);
+                SetupSketchObject(_sketchObjectPrefabs.chapter02, _obtainingObjectPrefabs.chapter02);
+                SetupSketchObject(_sketchObjectPrefabs.chapter03, _obtainingObjectPrefabs.chapter03).transform.Translate(1, 0, 0);
+                
+                /* SUSPENDED: 전시용으로 모든 챕터의 그림을 한번에 그리는 것으로 기획이 변경됨
+                 
                 // 챕터별로 그릴 오브젝트 생성
                 (GameObject sktech, GameObject obtaining) drawingPrefabPair = _gameManager.CurrentChapter switch
                 {
@@ -62,6 +69,7 @@ namespace Reless
                     _ => throw new ArgumentOutOfRangeException()
                 };
                 SetupSketchObject(drawingPrefabPair.sktech, drawingPrefabPair.obtaining);
+                */
             }
         }
 
@@ -70,7 +78,8 @@ namespace Reless
         /// </summary>
         /// <param name="sketchPrefab">그릴 오브젝트의 프리팹</param>
         /// <param name="obtainingObjectPrefab">얻을 오브젝트의 프리팹</param>
-        private void SetupSketchObject(GameObject sketchPrefab, GameObject obtainingObjectPrefab)
+        /// <returns>그릴 오브젝트</returns>
+        private GameObject SetupSketchObject(GameObject sketchPrefab, GameObject obtainingObjectPrefab)
         {
             // 그릴 오브젝트를 생성합니다.
             var sketchObject = Instantiate(sketchPrefab);
@@ -81,6 +90,8 @@ namespace Reless
             // 그릴 오브젝트가 그려졌을 때 얻을 오브젝트를 획득하게 합니다.
             sketchObject.GetComponent<SketchOutline>().DrawingCompleted +=
                 () => ObtainObject(sketchObject, obtainingObjectPrefab);
+            
+            return sketchObject;
         }
         
         /// <summary>
