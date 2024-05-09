3f90d480031d6ea41f926f642f41f4149d406c23
plwast-ints <plwast.ints@gmail.com>
2024-05-09 17:20:16 +0900

    Debug 모드에서만 실행하는 래핑 로그 Logger.cs 추가 - 기존에 테스트 폴더 아래에 있던 Logging.cs 및 스크립터블 오브젝트를 삭제하고 추가한 Logger.cs로 대체 - Logger로 이름이 변경된 이유는 로그 더블클릭 시 올바른 호출 위치로 가기 위해서입니다. 참고: https://www.reddit.com/r/Unity3D/comments/17eikh0/i_found_a_way_to_go_to_the_right_line_in_your/

---
+++ Unity-Re_less/Assets/Scripts/Logger.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using Conditional = System.Diagnostics.ConditionalAttribute;
+
+namespace Reless
+{
+    /// <summary>
+    /// 디버그 모드에서만 로그를 출력하는 래핑 클래스입니다.
+    /// </summary>
+    /// <remarks>
+    /// 씬과 무관하게 UnityEvent에서도 사용할 수 있도록 ScriptableObject를 이용합니다.
+    /// </remarks>
+    [CreateAssetMenu(fileName = "Logger", menuName = "Scriptable Object/Logger", order = int.MaxValue)]
+    public class Logger : ScriptableObject
+    {
+        // ReSharper disable Unity.PerformanceAnalysis
+        [Conditional("DEBUG")]
+        public static void Log(string message)
+        {
+            Debug.Log(message);
+        }
+        
+        // ReSharper disable Unity.PerformanceAnalysis
+        [Conditional("DEBUG")]
+        public static void LogWarning(string message)
+        {
+            Debug.LogWarning(message);
+        }
+        
+        // ReSharper disable Unity.PerformanceAnalysis
+        [Conditional("DEBUG")]
+        public static void LogError(string message)
+        {
+            Debug.LogError(message);
+        }
+        
+        // ReSharper disable Unity.PerformanceAnalysis
+        [Conditional("DEBUG")]
+        public static void LogException(System.Exception exception)
+        {
+            Debug.LogException(exception);
+        }
+        
+        // ReSharper disable Unity.PerformanceAnalysis
+        [Conditional("DEBUG")]
+        public static void LogFormat(string format, params object[] args)
+        {
+            Debug.LogFormat(format, args);
+        }
+        
+        // ReSharper disable Unity.PerformanceAnalysis
+        [Conditional("DEBUG")]
+        public static void LogWarningFormat(string format, params object[] args)
+        {
+            Debug.LogWarningFormat(format, args);
+        }
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        [Conditional("DEBUG")]
+        public static void LogErrorFormat(string format, params object[] args)
+        {
+            Debug.LogErrorFormat(format, args);
+        }
+        
+        // ReSharper disable Unity.PerformanceAnalysis
+        [Conditional("DEBUG")]
+        public static void Assert(bool condition, string message)
+        {
+            Debug.Assert(condition, message);
+        }
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        [Conditional("DEBUG")]
+        public static void AssertFormat(bool condition, string format, params object[] args)
+        {
+            Debug.AssertFormat(condition, format, args);
+        }
+    }
+}
