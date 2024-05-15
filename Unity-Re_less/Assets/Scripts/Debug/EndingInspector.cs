e70a64f1c9e9c9e082a1a6c4d619293ad7c81b9d
plwast-ints <plwast.ints@gmail.com>
2024-05-15 18:00:02 +0900

    Ending 관련 클래스들 및 씬 정리 - EndingInspector.cs 추가. 관련 이슈: #17 - EndingBootstrapper.cs에서 EndingLoader.cs / EndingRoomTransformer.cs로 분리 - Ending 씬 이전 커밋(c265b93bba9bf580aeb105b90a11bb41761a6e2d)   및 지금 커밋의 변경사항에 맞게 변경 - MainScene 또한 수정한 내용에 따라 조정

---
+++ Unity-Re_less/Assets/Scripts/Debug/EndingInspector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using Reless.MR;
+using UnityEngine;
+
+namespace Reless.Debug
+{
+    /// <summary>
+    /// 에디터에서 엔딩 씬에서 플레이할 경우 적절히 플레이가 시뮬레이션 되도록 도와주는 클래스입니다.
+    /// </summary>
+    public class EndingInspector : MonoBehaviour
+    {
+        private void Start()
+        {
+            // 게임 상태를 엔딩으로 변경합니다.
+            GameManager.CurrentPhase = GamePhase.Ending;
+
+            RoomManager.TryInvokeAction(_ => FixCameraPosition());
+        }
+
+        /// <summary>
+        /// 엔딩 씬의 디버그용 카메라는 변환과 관계 없이 항상 문에서 바깥쪽을 바라봐야 합니다.
+        /// </summary>
+        private void FixCameraPosition()
+        {
+            StartCoroutine(WaitForOneFrame());
+            
+            IEnumerator WaitForOneFrame()
+            {
+                // EndingRoomTransform가 트래킹 스페이스에 변환을 수행하는 동안 대기합니다.
+                yield return new WaitForEndOfFrame();
+                
+                Logger.Log($"{nameof(EndingInspector)}: Fixing camera position for testing.");
+                
+                // 트래킹 스페이스를 다시 초기화합니다.
+                GameManager.CameraRig.trackingSpace.localPosition = Vector3.zero;
+                GameManager.CameraRig.trackingSpace.localRotation = Quaternion.identity;
+            }
+        }
+    }
+}
