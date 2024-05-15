e280b78c70abd46d0418d9b1f8e0a536b9b42cb8
dkwjd-21 <137870180+dkwjd-21@users.noreply.github.com>
2024-05-16 04:01:28 +0900

    [VR] 챕터 2 문 높이 수정

--- Unity-Re_less/Assets/Scripts/VR Room Script/SetActive.cs
+++ Unity-Re_less/Assets/Scripts/VR Room Script/SetActive.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Reless.VR;
 
 namespace Reless
 {
     public class SetActive : MonoBehaviour
     {   
         public GameObject endingCharacters;
+
+        // Ending
+        [SerializeField]
+        EndingBehaviour _EndingBehaviour;
         
         // Start is called before the first frame update
         void Start()
@@ -20,6 +25,11 @@ namespace Reless
             if(endingCharacters.activeSelf)
             {
                 GetComponent<Renderer>().enabled = true;
+            }
+
+            if(_EndingBehaviour._isEndChatFin)
+            {
+                gameObject.SetActive(false);
             }
         }
     }
