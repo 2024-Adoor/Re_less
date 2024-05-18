8a45c90a79cf35bd92ec74b567aab68b44c78ccd
plwast-ints <plwast.ints@gmail.com>
2024-05-19 05:36:15 +0900

    챕터 2 시계 부수기를 컨트롤러로 부딪히는 것으로 변경

--- Unity-Re_less/Assets/Scripts/VR Room Script/Broken_Watch.cs
+++ Unity-Re_less/Assets/Scripts/VR Room Script/Broken_Watch.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Broken_Watch : MonoBehaviour
@@ -7,21 +6,20 @@ public class Broken_Watch : MonoBehaviour
     private int collisionCount = 0;     // 충돌 횟수를 저장할 변수
     public GameObject newPrefab1;  // 변경할 프리팹1
     public GameObject newPrefab2;  // 변경할 프리팹2
-    public Vector3 RotationOffset;              // 변경할 프리팹 위치 
-    public bool isBroken = false;
+    
+    /// <summary>
+    /// 때리기를 감지하기 위한 플레이어의 손(컨트롤러 포함)에 있는 콜라이더들
+    /// </summary>
+    [SerializeField]
+    private Collider[] handColliders;
 
     public Ch02ObjectSpawner[] ch02ObjectSpawners;
 
     GameObject newObject_1;
-
-    void Update()
+    
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
+        if (handColliders.Contains(other))
         {
             collisionCount++; // 충돌 횟수 증가
             Debug.Log("충돌 횟수: " + collisionCount);
@@ -33,13 +31,11 @@ public class Broken_Watch : MonoBehaviour
                 newObject_1 = Instantiate(newPrefab1, newPrefab1.transform.position, newPrefab1.transform.rotation);
             }
             // 충돌 횟수 2이면 프리팹 변경 2
-            if (collisionCount == 2)
+            else if (collisionCount == 2)
             {
                 Destroy(gameObject);
                 Destroy(newObject_1);
-                GameObject newObject_2 = Instantiate(newPrefab2, newPrefab2.transform.position, newPrefab2.transform.rotation);
-
-                isBroken = true;
+                Instantiate(newPrefab2, newPrefab2.transform.position, newPrefab2.transform.rotation);
 
                 foreach (var spawner in ch02ObjectSpawners) { spawner.StopSpawn(); }
             }
